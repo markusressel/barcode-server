5cf1aeca6726c864e81926daa0931e295d6f520e
Markus Ressel <mail@markusressel.de>
2022-03-09 00:08:19 +0100

    add support for unicode number input

--- barcode_server/keyevent_reader.py
+++ barcode_server/keyevent_reader.py
@@ -25,7 +25,7 @@ US_EN_UPPER_DICT = {
     "'": "\"",
     "\\": "|",
     "[": "{",
-    "]": "{"
+    "]": "}"
 }
 
 
@@ -38,6 +38,7 @@ class KeyEventReader:
         self._shift = False
         self._caps = False
         self._alt = False
+        self._unicode_number_input_buffer = ""
 
         self._line = ""
 
@@ -79,6 +80,7 @@ class KeyEventReader:
         if code in ["KEY_ENTER", "KEY_KPENTER"]:
             if state == KeyEvent.key_up:
                 # line is finished
+                self._reset_modifiers()
                 return True
         elif code in ["KEY_RIGHTSHIFT", "KEY_LEFTSHIFT"]:
             if state in [KeyEvent.key_down, KeyEvent.key_hold]:
@@ -90,13 +92,23 @@ class KeyEventReader:
                 self._alt = True
             else:
                 self._alt = False
+
+                character = self._unicode_numbers_to_character(self._unicode_number_input_buffer)
+                self._unicode_number_input_buffer = ""
+
+                if character is not None:
+                    self._line += character
+
         elif code == "KEY_BACKSPACE":
             self._line = self._line[:-1]
         elif state == KeyEvent.key_down:
             character = self._code_to_character(code)
-            if character is not None:
-                # append the current character
-                self._line += character
+            if self._alt:
+                self._unicode_number_input_buffer += character
+            else:
+                if character is not None and not self._alt:
+                    # append the current character
+                    self._line += character
 
         return False
 
@@ -162,3 +174,25 @@ class KeyEventReader:
             character = character.lower()
 
         return character
+
+    @staticmethod
+    def _unicode_numbers_to_character(code: str) -> chr or None:
+        if code is None or len(code) <= 0:
+            return None
+
+        try:
+            # convert to hex
+            i = int(code)
+            h = hex(i)
+            s = f"{h}"
+
+            return bytearray.fromhex(s[2:]).decode('utf-8')
+        except Exception as ex:
+            LOGGER.exception(ex)
+            return None
+
+    def _reset_modifiers(self):
+        self._alt = False
+        self._unicode_number_input_buffer = ""
+        self._shift = False
+        self._caps = False
